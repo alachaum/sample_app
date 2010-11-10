d4d93b4b2e8b98455d0ebe65c4f818e565fab11b
Arnaud <arnaud.lachaume@gmail.com>
2010-11-10 09:25:20 +0100

    Final additions of Chap 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  
+
+  #Define the logo to be printed in the header
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
+
   #Define a title on a per page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
