58eaf65bebad54011bf8c0b43e398820e2b5a382
Arnaud <arnaud.lachaume@gmail.com>
2010-11-21 08:54:29 +0100

    Signin - Signout added. Signup Signin as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,10 +36,15 @@ class User < ActiveRecord::Base
     self.encrypted_password == encrypt(submitted_password)
   end
 
-  def self.authenticate(email,submitted_password)
+  #Returns the user if ok, else nil
+  def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    (user && user.has_password?(submitted_password)) ? user : nil
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
   end
 
   
