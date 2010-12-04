3be201d8cb91500d02c736b50d2e70df7be48274
Arnaud <arnaud.lachaume@gmail.com>
2010-12-04 02:26:33 +0100

    Chapter 10 finished

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -5,10 +5,12 @@ namespace :db do
   desc "Fill database with sample users"
   task :populate => :environment do
     Rake::Task['db:reset'].invoke
-    User.create!(:name => "Example user",
+    admin = User.create!(:name => "Example user",
                  :email => "example@railstutorial.org",
                  :password => "foobar",
                  :password_confirmation => "foobar")
+    admin.toggle!(:admin)
+
     99.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@railstutorial.org"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -111,24 +111,43 @@ describe User do
       end
 
     end
+  end
     
-    describe "authenticate method" do
+  describe "authenticate method" do
 
-      it "should return nil on email/password mismatch" do
-        user_wrong_password = User.authenticate(@attr[:email], "invalid")
-        user_wrong_password.should be_nil
-      end
+    it "should return nil on email/password mismatch" do
+      user_wrong_password = User.authenticate(@attr[:email], "invalid")
+      user_wrong_password.should be_nil
+    end
 
-      it "should return nil if the user doesn't exist" do
-        user_non_existing = User.authenticate("nonexisting@nul.com", @attr[:password])
-        user_non_existing.should be_nil
-      end
+    it "should return nil if the user doesn't exist" do
+      user_non_existing = User.authenticate("nonexisting@nul.com", @attr[:password])
+      user_non_existing.should be_nil
+    end
 
-      it "should return the user object if on email/password match" do
-        user_valid = User.authenticate(@attr[:email], @attr[:password])
-        user_valid.should == @user
-      end
+    it "should return the user object if on email/password match" do
+      user_valid = User.authenticate(@attr[:email], @attr[:password])
+       user_valid.should == @user
     end
   end
 
+  describe "admin attribute" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible into an admin" do
+      @user.toggle(:admin)
+      @user.should be_admin
+    end
+  end
 end
