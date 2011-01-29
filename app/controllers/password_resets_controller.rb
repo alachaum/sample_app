65f46648c9ed7bb60ecf944370d98b22c572387c
Arnaud Lachaume <arnaud.lachaume@gmail.com>
2011-01-30 01:33:33 +1100

    Password reset functionality complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_filter :unsigned_user, :only => [:new, :create, :show, :edit, :update]
+  
   # GET /password_resets
   # GET /password_resets.xml
   def index
@@ -14,42 +16,40 @@ class PasswordResetsController < ApplicationController
   # GET /password_resets/1.xml
   def show
     @password_reset = PasswordReset.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @password_reset }
-    end
+    @title = "Password Reset"
   end
 
   # GET /password_resets/new
   # GET /password_resets/new.xml
   def new
     @password_reset = PasswordReset.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @password_reset }
-    end
+    @title = "Password Reset"
   end
 
   # GET /password_resets/1/edit
   def edit
     @password_reset = PasswordReset.find(params[:id])
+    
+    if @password_reset && @password_reset.active && @password_reset.token == params[:token]
+      @title = "Password Reset"
+    else
+      redirect_to root_path
+    end
   end
 
   # POST /password_resets
   # POST /password_resets.xml
   def create
-    @password_reset = PasswordReset.new(params[:password_reset])
+    @password_reset = PasswordReset.new(params[:password_reset])    
 
-    respond_to do |format|
-      if @password_reset.save
-        format.html { redirect_to(@password_reset, :notice => 'Password reset was successfully created.') }
-        format.xml  { render :xml => @password_reset, :status => :created, :location => @password_reset }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @password_reset.errors, :status => :unprocessable_entity }
-      end
+    if @password_reset.save
+      flash[:success] = "Your request has been processed successfully"
+      user = User.find(@password_reset.user_id)
+      UserMailer.password_reset_confirmation(@password_reset).deliver
+      redirect_to @password_reset
+    else
+      @title = "Password Reset"
+      render 'new'
     end
   end
 
@@ -57,15 +57,20 @@ class PasswordResetsController < ApplicationController
   # PUT /password_resets/1.xml
   def update
     @password_reset = PasswordReset.find(params[:id])
-
-    respond_to do |format|
-      if @password_reset.update_attributes(params[:password_reset])
-        format.html { redirect_to(@password_reset, :notice => 'Password reset was successfully updated.') }
-        format.xml  { head :ok }
+    token = params[:password_reset][:token]
+    
+    if @password_reset.active && token == @password_reset.token
+      if @password_reset.user.update_attributes(params[:user])
+        sign_in(@password_reset.user)
+        flash[:success] = "Your password has been reset successfully"
+        @password_reset.active = false
+        @password_reset.save
+        redirect_to root_path
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @password_reset.errors, :status => :unprocessable_entity }
+        render 'edit'
       end
+    else
+      redirect_to root_path
     end
   end
 
@@ -80,4 +85,9 @@ class PasswordResetsController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  private
+    def unsigned_user
+      redirect_to root_path if signed_in?
+    end
 end
